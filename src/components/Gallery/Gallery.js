5dca8f2dc8e267584656684981b0d72e137b9c6f
Adam Oliveras <theadamoliveras@gmail.com>
2019-09-12 17:47:57 -0700

    Started reactifying the Electra app. I've got a layout, navbar and gallery component working so far.

---
+++ src/components/Gallery/Gallery.js
@@ -0,0 +1,267 @@
+import React, { Component } from 'react'
+import fillerPic from './images/32.jpg';
+import './gallery.css';
+
+export default class Gallery extends Component {
+
+  render() {
+    return (
+      <div id="gallery-container">
+        <section class="gallery">
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h4 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v3">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h4 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v3">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h2 v2">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        <div class="item h1 v1">
+          <img src={fillerPic} alt="filler"/>
+          <div class="item__overlay">
+            <button>Take a closer <span>👀</span></button>
+          </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+} 
---
+++ src/components/common/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Navbar.css';
+import logo from './images/ElectraLogo.png';
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div id="navbar-container">
+        <header>
+          <nav>
+            <ul>
+              <li id="logo">
+                <a href="/"
+                  ><img
+                    id="logo-img"
+                    src={logo}
+                    alt="Electra Logo"
+                /></a>
+              </li>
+              <li id="critiques"><a href="/">Critiques</a></li>
+              <li id="gallery"><a href="/">Gallery</a></li>
+              <li id="profile"><a href="pages/userSignup.html">Signup</a></li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+    )
+  }
+}
