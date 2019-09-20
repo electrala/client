786ad655353a518bc0b62dd0da9363d2daedfb30
Brennan Newton <brennanmnewton@gmail.com>
2019-09-20 08:46:27 -0700

    Modal refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import './css/style.css';
+import '../css/style.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Layout from './components/common/Layout/Layout';
-import Gallery from './components/Gallery/Gallery';
-import Modal from '../Modal/Modal';
+import Layout from './common/Layout/Layout';
+import Gallery from './Gallery/Gallery';
+import Modal from './Modal/Modal';
 
 class App extends React.Component {
   constructor(props) {
@@ -15,21 +15,23 @@ class App extends React.Component {
   }
 
   handleClick = event => {
-    console.log('Button clicked!');
     event.preventDefault();
+    console.log('Button clicked!');
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
 
-  return (
-     <Layout>
-       <Modal show={this.state.isOpen} onClose={this.handleClick}>
-          Modal content
-       </Modal>
-       <Gallery />
-     </Layout>
-  );
+  render() {
+    return (
+       <Layout>
+         <Modal show={this.state.isOpen} onClick={this.handleClick}>
+            Modal content
+         </Modal>
+         <Gallery />
+       </Layout>
+    );
+  }
 }
 
 export default App;
