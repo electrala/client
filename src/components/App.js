c27fa0639a35fc0dc6764580eac879d28f533ac1
Brennan Newton <brennanmnewton@gmail.com>
2019-10-16 15:29:49 -0700

    App.js documentation

--- src/components/App.js
+++ src/components/App.js
@@ -15,12 +15,15 @@ class App extends React.Component {
     super(props);
     this.state = {
       showCrit: false,
-      showUser: false,
       showLogin: false,
       critiques: [],
     };
   }
 
+  /**
+   * The following 2 functions toggle the critique upload modal.
+   * @param {object} event This is the event triggered by clicking the plus sign button
+   */
   showCritModal = event => {
     event.preventDefault();
     this.setState({
@@ -35,19 +38,10 @@ class App extends React.Component {
     })
   }
 
-  showUserModal = event => {
-    this.setState({
-      showUser: true,
-    });
-  }
-
-
-  closeUserModal = event => {
-    this.setState({
-      showUser: false,
-    });
-  }
-
+  /**
+   * The following 2 function toggle the signup/login modal.
+   * @param {object} event This is the event triggered by clicking the signup/login button
+   */
   showLoginModal = event => {
     this.setState({
       showLogin: true, 
@@ -61,6 +55,12 @@ class App extends React.Component {
   }
 
 
+  /**
+   * Uploads a critique to our critiques table on postgres.
+   * Pushes the new critiques to the criques array. (This array isn't currently
+   * being used, but it might be useful for rendering critiques.)
+   * @param {object} data This is the data from the critique upload form
+   */
   uploadCrit = async data => {
     const new_crit = await axios.post('http://localhost:5000/critiques/new', data);
     const crits = this.state.critiques;
@@ -70,12 +70,22 @@ class App extends React.Component {
     })
   }
 
+  /**
+   * Adds a user to our users table on postgres.
+   * Logs a user in.
+   * @param {object} data This is the data from the sign up form
+   */
   signUp = async data => {
     const new_user = await axios.post('http://localhost:5000/users/register', data);
     const new_user_data = JSON.parse(new_user.config.data);
     this.logIn(new_user_data);
   }
 
+  /**
+   * Checks to see if a user is in our users table and the passwords match.
+   * If both are true, then the JWT is stored in local storage.
+   * @param {object} data This is the data from the log in form
+   */
   logIn = async data => {
     const result = await axios.post('http://localhost:5000/users/login', data);
     const token = result.data.token;
@@ -83,6 +93,11 @@ class App extends React.Component {
     this.setToken(token);
   }
 
+  /**
+   * Sets the Authorization header to the JWT. This header will be used for
+   * validation on the back end.
+   * @param {string} token This is the JSON Web Token
+   */
   setToken = (token = null) => {
     let tempToken = token;
     if (tempToken !== null) tempToken = localStorage.getItem('jwt');
@@ -96,16 +111,11 @@ class App extends React.Component {
         <Modal show={this.state.showCrit} onClose={this.closeCritModal}>
           <UploadCrit onUpload={this.uploadCrit} />
         </Modal>
-        <Modal show={this.state.showUser} onClose={this.closeUserModal}>
-          <Signup createUser={this.signUp}/>
-        </Modal>
         <Modal show={this.state.showLogin} onClose={this.closeLoginModal}>
           <Login loginUser={this.logIn}/>
           {/* <div className="line-container"></div> */}
           <Signup signUp={this.signUp}/>
         </Modal>
-
-        
         <div id="float-button">
           <button onClick={this.showCritModal}>
             <img src={require('./plusSign.png')} alt="plus sign for upload" />
