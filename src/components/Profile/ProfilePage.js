6659d914100cac9d26e763888cc8b6efbea2be3c
Ayana Hawk <ayanahawk11@gmail.com>
2019-10-22 09:05:05 -0700

    workign on css of profile page

--- src/components/Profile/ProfilePage.js
+++ src/components/Profile/ProfilePage.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react';
+import Pic from './pic.png'
 
 
 export default class ProfilePage extends Component {
 
- 
-   
+
+
     render() {
         return (
             <div className="user">
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
 
-               <h2>Profile</h2>
+                <div className="container">
+                    <div>
+                        <img className="photo" src={Pic} alt="profile pic" ></img>
+                    </div>
+
+                    <div className="row">
+
+                        <div className="left col-lg-4">
+                            <div >
+                                <ul className="nav">
+                                    <li>Gallery</li>
+                                    <li>Collections</li>
+                                    <li>About</li>
+                                </ul>
+                                <span className="follow">Follow</span>
+
+                                <div className="active"></div>
+                            </div>
+
+                            <h4 className="name">Ayana Hawk</h4>
+                            <br />
+                            <p className="info">UI/UX Designer</p>
+                            <br />
+                            <p className="info">Los Angeles</p>
+
+                            <div className="stats row">
+                                <div className="stat col-xs-4" >
+
+
+                                    <p className="number-stat">3,619</p>
+                                    <p className="desc-stat">Followers</p>
+                                </div>
+                                <div className="stat col-xs-4">
+                                    <p classNameName="number-stat">42</p>
+                                    <p className="desc-stat">Following</p>
+                                </div>
+                                <div className="stat col-xs-4" >
+                                    <p className="number-stat">38</p>
+                                    <p className="desc-stat">Uploads</p>
+                                </div>
+                            </div>
+                            <p className="desc">Hi ! My name is Ayana I'm a UI/UX Designer from LA</p>
+
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
