573a48d844d7931481760f371e7bf2a5278118ea
Adam Oliveras <theadamoliveras@gmail.com>
2019-10-22 18:26:35 -0700

    Now have the file being selected in uploadCrit component effectively being saved to the s3 bucket I've created. Next step is to get the url location saved to the sql database. Changes made to the uploadCrit component mainly.

--- src/components/UploadCrit/UploadCrit.js
+++ src/components/UploadCrit/UploadCrit.js
@@ -1,71 +1,82 @@
-import React from 'react';
-import './UploadCrit.css';
-import axios from 'axios';
+import React from "react";
+import "./UploadCrit.css";
+import axios from "axios";
 
 export default class UploadCrit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'b',
-      title: '',
-      genre: '',
-      description: '',
-      questions: '',
-      selectedFile: null,
-      selectedFiles: null
-    }
+      username: "b",
+      title: "",
+      genre: "",
+      description: "",
+      questions: "",
+      selectedFile: null
+    };
   }
 
-  singleFileChangedHandler = ( event ) => {
+  singleFileChangedHandler = event => {
     this.setState({
-     selectedFile: event.target.files[0]
+      selectedFile: event.target.files[0]
     });
-   };
+  };
 
-   singleFileUploadHandler = (	) => {
+  singleFileUploadHandler = () => {
     const data = new FormData();
-  // If file selected
-    if ( this.state.selectedFile ) {
-  data.append( 'critiqueImage', this.state.selectedFile, this.state.selectedFile.name );
-  axios.post( '/api/critique/critique-img-upload', data, {
-      headers: {
-       'accept': 'application/json',
-       'Accept-Language': 'en-US,en;q=0.8',
-       'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
-      }
-     })
-      .then( ( response ) => {
-  if ( 200 === response.status ) {
-        // If file size is larger than expected.
-        if( response.data.error ) {
-         if ( 'LIMIT_FILE_SIZE' === response.data.error.code ) {
-          this.ocShowAlert( 'Max size: 2MB', 'red' );
-         } else {
-          console.log( response.data );
-  // If not the given file type
-          this.ocShowAlert( response.data.error, 'red' );
-         }
-        } else {
-         // Success
-         let fileName = response.data;
-         console.log( 'fileData', fileName );
-         this.ocShowAlert( 'File Uploaded', '#3089cf' );
-        }
-       }
-      }).catch( ( error ) => {
-      // If another error
-      this.ocShowAlert( error, 'red' );
-     });
+    // If file selected
+    if (this.state.selectedFile) {
+      data.append(
+        "critiqueImage",
+        this.state.selectedFile,
+        this.state.selectedFile.name
+      );
+      axios
+        .post('http://localhost:5000/aws/critique-img-upload', data, {
+          headers: {
+            accept: "application/json",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Content-Type": `multipart/form-data; boundary=${data._boundary}`
+          }
+        })
+        .then(response => {
+          if (200 === response.status) {
+            // If file size is larger than expected.
+            if (response.data.error) {
+              if ("LIMIT_FILE_SIZE" === response.data.error.code) {
+                // this.ocShowAlert("Max size: 2MB", "red");
+                console.error('file too big');
+              } else {
+                console.log(response.data);
+                // If not the given file type
+                // this.ocShowAlert(response.data.error, "red");
+                console.error(response.data.error);
+              }
+            } else {
+              // Success
+              let fileName = response.data;
+              console.log("fileData", fileName);
+              // this.ocShowAlert("File Uploaded", "#3089cf");
+              console.log('File uploaded!');
+            }
+          }
+        })
+        .catch(error => {
+          // If another error
+          // this.ocShowAlert(error, "red");
+          console.error(error);
+        });
     } else {
-     // if file not selected throw error
-     this.ocShowAlert( 'Please upload file', 'red' );
+      // if file not selected throw error
+      // this.ocShowAlert("Please upload file", "red");
+      console.log('Please upload file!');
     }
   };
 
   handleSubmit = event => {
     event.preventDefault();
+    // insert async singlefileuploadhandler here?
     this.props.onUpload(this.state);
-  }
+  };
 
   handleChange = event => {
     const { target } = event;
@@ -73,31 +84,33 @@ export default class UploadCrit extends React.Component {
     const { name } = target;
 
     this.setState({
-      [name]: value,
+      [name]: value
     });
   };
 
   render() {
     return (
       <div className="upload-form">
-        <form id="upload-form"  onSubmit={this.handleSubmit}>
+        <form id="upload-form" onSubmit={this.handleSubmit}>
           <label id="form-title">Upload Critique</label>
           <label id="title-label">Title</label>
           <label id="genre-label">Genre</label>
-          <input 
+          <input
             type="text"
             name="title"
             id="title"
             value={this.state.title}
             onChange={this.handleChange}
           ></input>
-          <select 
-            name="genre" 
+          <select
+            name="genre"
             id="genre"
             value={this.state.genre}
             onChange={this.handleChange}
           >
-            <option value="" disable selected>Select a genre</option>
+            <option value="" disable selected>
+              Select a genre
+            </option>
             <option value="video">Video</option>
             <option value="photography">Photography</option>
             <option value="digital">Digital Art</option>
@@ -120,9 +133,17 @@ export default class UploadCrit extends React.Component {
             value={this.state.questions}
             onChange={this.handleChange}
           ></textarea>
-          <button className="submit" type="submit">Upload</button>
+          <div className="file-upload-container">
+            <input type="file" name="critique-image" id="critique-image"
+              onChange={this.singleFileChangedHandler}
+            />
+            <button onClick={this.singleFileUploadHandler}>Upload File</button>
+          </div>
+          <button className="submit" type="submit">
+            Submit Ya Crit!
+          </button>
         </form>
       </div>
-    )
+    );
   }
 }
