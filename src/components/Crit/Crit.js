e5b3f83772cd8e57314d4d9c28d02e141d220bfc
Adam Oliveras <theadamoliveras@gmail.com>
2019-10-29 14:49:32 -0700

    Added ReactModal component to Crit.js component, next step will be to style the modal component.

--- src/components/Crit/Crit.js
+++ src/components/Crit/Crit.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactModal from 'react-modal';
 
 export default class Crit extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor() {
+    super();
+    this.state = {
+      showModal: false
+    };
+
+    this.handleOpenModal = this.handleOpenModal.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+  }
+
   randomNumber = (limit) =>
     Math.floor(Math.random() * limit) + 1;
+
+  handleOpenModal() {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal() {
+    this.setState({ showModal: false });
+  }
 
   render() {
     const { username, title, description, questions, genre, s3locationurl } = this.props.critique;
@@ -13,9 +29,22 @@ export default class Crit extends Component {
       <div className={`item h${this.randomNumber(4)} v${this.randomNumber(4)}`} >
         <img src={s3locationurl} alt="filler" />
         <div className="item__overlay">
-          <button>
+          {/* <button>
             Take a closer <span role="img">👀</span>
-          </button>
+          </button> */}
+          <button onClick={this.handleOpenModal}>Check me out!</button>
+          <ReactModal
+            isOpen={this.state.showModal}
+            contentLabel="Modal #1 Global Style Override Example"
+            onRequestClose={this.handleCloseModal}
+          >
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <p>{genre}</p>
+            <p>{questions}</p>
+            <img src={s3locationurl} alt="critique photo" style={{ maxWidth: '200px' }} />
+            <button onClick={this.handleCloseModal}>Close Modal</button>
+          </ReactModal>
         </div>
       </div >
     )
