6990810c1b13c451f5e10ba4ba9be5da087e89a6
Adam Oliveras <theadamoliveras@gmail.com>
2019-10-29 16:57:33 -0700

    Started grid css version of styling for critique-modal component. Need to continue tweaking but getting closer. Changes made in Crit.js and created Crit.css and began styling in that file.

--- src/components/Crit/Crit.js
+++ src/components/Crit/Crit.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactModal from 'react-modal';
+import './Crit.css';
 
 export default class Crit extends Component {
   constructor() {
@@ -37,13 +38,42 @@ export default class Crit extends Component {
             isOpen={this.state.showModal}
             contentLabel="Modal #1 Global Style Override Example"
             onRequestClose={this.handleCloseModal}
+            style={{
+              overlay: {
+                position: 'fixed',
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                backgroundColor: 'rgba(255, 255, 255, 0.75)'
+              },
+              content: {
+                position: 'absolute',
+                top: '20%',
+                left: '20%',
+                right: '20%',
+                bottom: '15%',
+                border: '1px solid #ccc',
+                background: '#fff',
+                overflow: 'auto',
+                WebkitOverflowScrolling: 'touch',
+                borderRadius: '20px',
+                outline: 'none',
+                padding: '20px',
+                boxShadow: '0px 4px 7px 0px rgba(0, 0, 0, 0.34)'
+
+              }
+            }}
           >
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <p>{genre}</p>
-            <p>{questions}</p>
-            <img src={s3locationurl} alt="critique photo" style={{ maxWidth: '200px' }} />
-            <button onClick={this.handleCloseModal}>Close Modal</button>
+            <div id="critique-modal-view" >
+              <h2 id="critique-title" >Title: {title}</h2>
+              <p id="critique-username">Created by: {username}</p>
+              <p id="critique-description" >Description: {description}</p>
+              {/* <p id="critique-genre" >{genre}</p> */}
+              <p id="critique-question" >I'd like to know: {questions}</p>
+              <img id="critique-modal-img" src={s3locationurl} alt="critique-modal-view" />
+              <button id="modal-close-button" onClick={this.handleCloseModal}>Close Modal</button>
+            </div>
           </ReactModal>
         </div>
       </div >
