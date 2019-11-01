29eee478d88535ed98ae4db1277bfe1d88bcc927
Adam Oliveras <theadamoliveras@gmail.com>
2019-11-01 15:00:45 -0700

    Changed profile state in app.js to false.

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
       showCrit: false,
       showLogin: false,
       critiques: [],
-      profilePic: true,
+      profilePic: false,
       userInfo: {}
     };
   }
