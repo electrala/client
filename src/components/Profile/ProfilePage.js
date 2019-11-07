58c4dc162a5839a8e45fef1163c0d0c2a52bbade
Ayana Hawk <ayanahawk11@gmail.com>
2019-11-07 14:48:36 -0800

    added profile card

--- src/components/Profile/ProfilePage.js
+++ src/components/Profile/ProfilePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import Pic from './bank_profile.png';
-import header from './newHeader.PNG';
+import header from './elect-banner.png';
 
 export default class ProfilePage extends Component {
     constructor(props) {
@@ -44,65 +44,55 @@ export default class ProfilePage extends Component {
         const { firstname, lastname, username, pronoun, location, email } = this.props.userInfo;
         return (
             <div className="user">
+                <img className='wrapper-img' src={header} alt="Electra Header" />
 
-                <div className="user-card">
 
-                    <div id="wrapper">
-                    <div><img   id='wrapper-img' src={header} alt="Electra Header" />
+                <div className="user-container">
+                    <img className="profilePhoto" src={Pic} alt="profilepic"></img>
+
+                    <h4 className="userName"> {firstname} {lastname}</h4>
+                    <p className="userPronoun"> {pronoun}</p>
+                    <p className="userLocation">{location}</p>
+
+                    <button className="editButton">
+                        Edit Profile
+</button>
+                </div>
+
+
+
+
+
+                <div className="left item2" >
+                    <ul className="profileNav">
+                        <li>About</li>
+                        <li>Gallery</li>
+                        <li>Collections</li>
+                    </ul>
+                    <div className="active"></div>
+                    <div className="row">
+                        <br />
                     </div>
-                    </div>
-
-                    <div className="container">
-
-                    <div className="img item1">
-                        <img className="profilePhoto" src={Pic} alt="profilepic"></img>
-
-                        <div className="userInfo item3">
-                            <h4 className="info">name: {firstname} {lastname}</h4>
-                            <br />
-                            <p className="info">username: {username}</p>
-                            <br />
-                            <p className="info">pronoun: {pronoun}</p>
-                            <br />
-                            <p className="info">location: {location}</p>
-                            <br />
-                            <p className="info">email: {email}</p>
+                    <div className="stats row item4">
+                        <div className="stat col-xs-4" >
+                            <p className="number-stat">3,619</p>
+                            <p className="desc-stat">Followers</p>
                         </div>
-                    </div>
-
-                
-                        
-                        <div className="left item2" >
-                            <ul className="profileNav">
-                                <li>About</li>
-                                <li>Gallery</li>
-                                <li>Collections</li>
-                            </ul>
-                            <div className="active"></div>
-                            <div className="row">
-                                <br />
-                            </div>
-                            <div className="stats row item4">
-                                <div className="stat col-xs-4" >
-                                    <p className="number-stat">3,619</p>
-                                    <p className="desc-stat">Followers</p>
-                                </div>
-                                <div className="stat col-xs-4">
-                                    <p className="number-stat">42</p>
-                                    <p className="desc-stat">Following</p>
-                                </div>
-                                <div className="stat col-xs-4" >
-                                    <p className="number-stat">38</p>
-                                    <p className="desc-stat">Uploads</p>
-                                </div>
-                                <div>
-                                    {this.renderRedirect()}
-                                    <button onClick={this.redirectLogout}>logout</button>
-                                </div>
-                            </div>
+                        <div className="stat col-xs-4">
+                            <p className="number-stat">42</p>
+                            <p className="desc-stat">Following</p>
+                        </div>
+                        <div className="stat col-xs-4" >
+                            <p className="number-stat">38</p>
+                            <p className="desc-stat">Uploads</p>
+                        </div>
+                        <div>
+                            {this.renderRedirect()}
+                            <button onClick={this.redirectLogout}>logout</button>
                         </div>
                     </div>
                 </div>
+
             </div>
 
 
