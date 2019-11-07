41c4de3a4945910861997fdfc3252a454d163e85
Dandy <dandyjreyes@gmail.com>
2019-11-07 10:53:37 -0800

    Adding local and heroku links to the App.js for ease of debugging

--- src/components/App.js
+++ src/components/App.js
@@ -85,7 +85,8 @@ class App extends React.Component {
   uploadCrit = async data => {
     try {
       const new_crit = await axios.post(
-        "https://electra-la-2019.herokuapp.com/critiques/new",
+        // "https://electra-la-2019.herokuapp.com/critiques/new",
+        "http://localhost:5000/critiques/new",
         data
       );
       const crits = this.state.critiques;
@@ -141,8 +142,8 @@ class App extends React.Component {
   logIn = async data => {
     try {
       const result = await axios.post(
-        "https://electra-la-2019.herokuapp.com/users/login",
-        // "http://localhost:5000/users/login",
+        // "https://electra-la-2019.herokuapp.com/users/login",
+        "http://localhost:5000/users/login",
         data
       );
       const token = result.data.token;
@@ -166,8 +167,8 @@ class App extends React.Component {
         const token = localStorage.getItem("jwt");
         const decoded = jwt_decode(token);
         const { data } = await axios.get(
-          `https://electra-la-2019.herokuapp.com/users/user/${decoded.id}`
-          // `http://localhost:5000/users/user/${decoded.id}`
+          // `https://electra-la-2019.herokuapp.com/users/user/${decoded.id}`
+          `http://localhost:5000/users/user/${decoded.id}`
           );
         delete data.password;
         this.setState({userInfo:data});
