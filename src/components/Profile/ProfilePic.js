01befb34abcdad68e9a8ffeb957d62d09fefe11c
Adam Oliveras <theadamoliveras@gmail.com>
2019-11-25 14:56:11 -0800

    Made required conflict changes and merged ayana's branch into mine.

--- src/components/Profile/ProfilePic.js
+++ src/components/Profile/ProfilePic.js
@@ -1,17 +1,16 @@
-import React, {Component} from 'react'; 
+import React, { Component } from 'react';
 import Pic from './bank_profile.png'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Profile.css';
 
 
-export default class ProfilePc extends Component{
-    constructor (props) {
+export default class ProfilePc extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-          isAuthenticated: false,
-          userData:false,
+            isAuthenticated: false,
+            userData: false,
         }
-<<<<<<< HEAD
     }
 
     render() {
@@ -22,18 +21,6 @@ export default class ProfilePc extends Component{
                     </img>
                 </Link>
             </div>
-=======
-      }
-      
-    render(){
-        return(
-        <div id="picture">
-            <Link to='/profile'>
-                <img id='profileImg' src={Pic} alt='pic'>
-                </img>
-            </Link>
-        </div>
->>>>>>> 439ab673d939dff4a8ec1b5409e3041765b27435
         )
     }
 }
